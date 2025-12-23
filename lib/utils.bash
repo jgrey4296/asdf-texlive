f308bf19f74a375657baa3777509518c29655371
John Grey <johngrey4296@gmail.com>
2025-12-23 17:38:08 +0000

    [refactor]:

--- lib/utils.bash
+++ lib/utils.bash
@@ -17,9 +17,9 @@ set -euo pipefail
 GH_REPO="https://github.com/jgrey4296/asdf-texlive.git"
 TOOL_NAME="texlive"
 TOOL_TEST="tlmgr --help"
-BIN_DIR="bin/x86_64-linux"
+BIN_DIR="bin/$(uname -m)-$(uname | tr '[:upper:]' '[:lower:]')"
 
-# TODO: list versions from here, and then download the respective instal-tl-unx.tar.gz
+# TODO: list versions from here, and then download the respective install-tl-unx.tar.gz
 TEXLIVE_ARCHIVE="https://ftp.math.utah.edu/pub/tex/historic/systems/texlive/"
 INSTALL_SCRIPT="install-tl"
 INSTALL_SCRIPT_URL="https://mirror.ctan.org/systems/texlive/tlnet"
@@ -31,7 +31,6 @@ fail() {
 
 curl_opts=(-fsSL)
 
-# NOTE: You might want to remove this if <YOUR TOOL> is not hosted on GitHub releases.
 if [ -n "${GITHUB_API_TOKEN:-}" ]; then
 	curl_opts=("${curl_opts[@]}" -H "Authorization: token $GITHUB_API_TOKEN")
 fi
@@ -42,59 +41,5 @@ function sort_versions() {
 }
 
 function list_all_versions() {
-	# TODO: Adapt this. By default we simply list the tag names from GitHub releases.
-	echo "2025"
-}
-
-function download_release() {
-	local version filename url
-	version="$1"
-	filename="$2"
-
-	url="$INSTALL_SCRIPT_URL/$filename"
-
-	echo "* Downloading $TOOL_NAME release $version as $filename..."
-	curl "${curl_opts[@]}" -o "$ASDF_DOWNLOAD_PATH/$filename" -C - "$url" || fail "Could not download $url"
-
-}
-
-function install_version() {
-	local install_type="$1"
-	local version="$2"
-	local install_path="$3"
-
-
-	if [ "$install_type" != "version" ]; then
-		fail "asdf-$TOOL_NAME supports release installs only"
-	fi
-
-	# if texuserdir is set else:
-	TEXUSERDIR="$XDG_CONFIG_HOME/texlive"
-
-	echo "* Running Installer..."
-	(
-		mkdir -p "$install_path/user/texmf-var"
-
-
-		perl "$ASDF_DOWNLOAD_PATH/$INSTALL_SCRIPT" \
-		--no-interaction \
-		-scheme=minimal \
-		-texdir "$install_path" \
-		-texmflocal "$install_path/texmf-local" \
-		-texmfsysvar "$install_path/texmf-var" \
-		-texmfsysconfig "$install_path/texmf-config" \
-		-texmfhome "$install_path/user" \
-		-texmfvar "$install_path/user/texmf-var" \
-		-texmfconfig "$TEXUSERDIR"
-
-
-		local tool_cmd
-		tool_cmd="$(echo "$TOOL_TEST" | cut -d' ' -f1)"
-		test -x "$install_path/$BIN_DIR/$tool_cmd" || fail "Expected $install_path/$BIN_DIR/$tool_cmd to be executable."
-
-		echo "$TOOL_NAME $version installation was successful!"
-	) || (
-		rm -rf "$install_path"
-		fail "An error occurred while installing $TOOL_NAME $version."
-	)
+	date +"%Y"
 }
